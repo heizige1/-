9c6daced03de7c6a757363c062e9c4c1c42819e9
王鑫 <919323640@qq.com>
2020-11-20 01:32:47 +0800

    practice

---
+++ text_11_20/text_11_20/源.c
@@ -0,0 +1,43 @@
+
+#include<stdio.h>
+
+
+
+
+
+
+//int  get_max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//
+//	return y;
+//}
+//
+//int main()
+//{
+//	int a, b;
+//	scanf_s("%d %d", &a, &b);
+//	int max = get_max(a, b);
+//	printf("max=%d\n", max);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a, b;
+//	scanf_s("%d %d", &a, &b);
+//	if (a > b)
+//	
+//		printf("%d", a);
+//		
+//	else if (a < b)
+//
+//		printf("%d", b);
+//
+//	else if (a = b)
+//		printf("两数相等\n");
+//
+//	
+//	return 0;
+//}
