ac50d690bd859f62a917095b1e19cd06dd47f2d6
王鑫 <919323640@qq.com>
2021-02-04 03:07:16 +0800

    prantice

---
+++ text_2_4/text_2_4/text.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;
+//	int n = 0;
+//	
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		int ret = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			ret = ret*j;
+//		}
+//		sum = sum + ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+int main()
+{
+	int a = 20;
+	int b = 10;
+	return 0;
+}
